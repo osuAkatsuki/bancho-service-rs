7e86f4257237aaa35771491701903c829f2ba2ba
infernalfire72 <jkoe2502@gmail.com>
2025-05-27 07:12:21 +0200

    add logout info log

--- src/events/logout.rs
+++ src/events/logout.rs
@@ -4,10 +4,16 @@ use crate::models::sessions::Session;
 use crate::repositories::streams::StreamName;
 use crate::usecases::{sessions, streams};
 use bancho_protocol::messages::server::UserLogout;
+use tracing::info;
 
 pub async fn handle(ctx: &RequestContext, session: &Session) -> EventResult {
     sessions::delete(ctx, session).await?;
     let logout_notification = UserLogout::new(session.user_id as _);
     streams::broadcast_message(ctx, StreamName::Main, logout_notification, None, None).await?;
+
+    info!(
+        user_id = session.user_id,
+        "User logged out."
+    );
     Ok(None)
 }
