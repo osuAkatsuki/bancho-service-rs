d82f9c8a3d579fc8d6b2d415913eac7b7454d32f
infernalfire72 <jkoe2502@gmail.com>
2025-06-10 20:31:56 +0200

    helm chart & k8s cronjob

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,3 +1,4 @@
+mod health;
 pub mod osu;
 pub mod v1;
 
@@ -11,7 +12,7 @@ use async_trait::async_trait;
 use axum::Router;
 use axum::extract::FromRequestParts;
 use axum::http::request::Parts;
-use axum::routing::post;
+use axum::routing::{get, post};
 use sqlx::{MySql, Pool};
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
@@ -21,7 +22,10 @@ pub async fn serve(settings: &AppSettings, state: AppState) -> anyhow::Result<()
     let addr = SocketAddr::from((settings.app_host, settings.app_port));
     info!("Listening on {addr}");
     let listener = TcpListener::bind(addr).await?;
-    let app = Router::new().merge(router()).with_state(state);
+    let app = Router::new()
+        .merge(router())
+        .route("/_health", get(health::health_check))
+        .with_state(state);
     axum::serve(
         listener,
         app.into_make_service_with_connect_info::<SocketAddr>(),
