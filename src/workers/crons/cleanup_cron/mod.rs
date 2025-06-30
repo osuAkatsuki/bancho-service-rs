cf2e1a3fe0661286a04e05e765d0c46494fb9b5c
infernalfire72 <jkoe2502@gmail.com>
2025-06-30 22:16:48 +0200

    move state init to component so each component can define their own state

--- src/workers/crons/cleanup_cron/mod.rs
+++ src/workers/crons/cleanup_cron/mod.rs
@@ -1,11 +1,13 @@
 pub mod tasks;
 
-use crate::common::state::AppState;
+use crate::common::init;
 use crate::cron_tasks;
+use crate::settings::AppSettings;
 use tasks::cleanup_sessions::cleanup_sessions;
 use tasks::cleanup_streams::cleanup_streams;
 
-pub async fn serve(ctx: AppState) -> anyhow::Result<()> {
+pub async fn serve(settings: &AppSettings) -> anyhow::Result<()> {
+    let ctx = init::initialize_state(settings).await?;
     cron_tasks! {
         &ctx,
         cleanup_sessions,
