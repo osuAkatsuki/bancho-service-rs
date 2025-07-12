1547d90ce611c2042ff04a7dafd60e2979a94606
infernalfire72 <jkoe2502@gmail.com>
2025-07-12 04:39:56 +0200

    add toggle maintenance usecase + command

--- src/commands/system.rs
+++ src/commands/system.rs
@@ -1,12 +1,23 @@
 use crate::commands;
 use crate::commands::{CommandResult, CommandRouterFactory};
 use crate::common::context::Context;
+use crate::models::privileges::Privileges;
 use crate::models::sessions::Session;
+use crate::usecases::bancho_settings;
 use bancho_service_macros::command;
 
 pub static COMMANDS: CommandRouterFactory = commands![maintenance];
 
-#[command("maintenance")]
-pub async fn maintenance<C: Context>(_ctx: &C, _sender: &Session) -> CommandResult {
-    Ok(todo!())
+#[command(
+    "maintenance",
+    required_privileges = Privileges::AdminCaker,
+)]
+pub async fn maintenance<C: Context>(ctx: &C, _sender: &Session) -> CommandResult {
+    let is_active = bancho_settings::toggle_maintenance(ctx).await?;
+    // TODO: kick all non-admin users unless they're in a multiplayer match
+    let on_off = match is_active {
+        true => "on",
+        false => "off",
+    };
+    Ok(format!("Turned {on_off} maintenance mode."))
 }
---
+++ src/usecases/bancho_settings.rs
@@ -0,0 +1,15 @@
+use crate::common::context::Context;
+use crate::common::error::ServiceResult;
+use crate::repositories::bancho_settings;
+
+const MAINTENANCE_KEY: &str = "bancho_maintenance";
+pub async fn toggle_maintenance<C: Context>(ctx: &C) -> ServiceResult<bool> {
+    let current = bancho_settings::fetch(ctx, MAINTENANCE_KEY).await?;
+    let is_active = current.value_int != 0;
+    let new_value = match is_active {
+        true => 0,
+        false => 1,
+    };
+    bancho_settings::update_int(ctx, MAINTENANCE_KEY, new_value).await?;
+    Ok(!is_active)
+}
