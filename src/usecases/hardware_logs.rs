3437536b842eb0e7622d668eaabc7ba2c4f9d01a
infernalfire72 <jkoe2502@gmail.com>
2025-08-29 17:32:59 +0200

    fix profile links on discord embeds

--- src/usecases/hardware_logs.rs
+++ src/usecases/hardware_logs.rs
@@ -1,6 +1,7 @@
 use crate::adapters::discord;
 use crate::common::context::Context;
 use crate::common::error::{AppError, ServiceResult, unexpected};
+use crate::common::website;
 use crate::models::bancho::ClientHashes;
 use crate::models::hardware_logs::{
     AggregateHardwareInfo, AggregateHardwareMatch, AggregateMatchingHardwareResult,
@@ -88,11 +89,14 @@ async fn do_verification_checks<C: Context>(
     username: &str,
     hw_matches: AggregateMatchingHardwareResult,
 ) -> ServiceResult<()> {
+    let user_profile_link = website::get_profile_link(user_id);
     // users::ban(ctx, user_id).await?;
-    let notification = format!("[{username}]({user_id})");
+    let notification =
+        format!("[{username}]({user_profile_link}) has been banned for creating a multiaccount.");
     let _ = discord::send_logs_red_embed("User banned (Multiaccount)", &notification, None).await;
     for (match_user_id, hw_match) in hw_matches.user_matches {
         let match_username = &hw_match.username;
+        let match_profile_link = website::get_profile_link(match_user_id);
         if hw_match.has_activated_hardware {
             match hw_match.user_privileges.is_publicly_visible() {
                 true => {
@@ -101,14 +105,14 @@ async fn do_verification_checks<C: Context>(
                     // users::restrict(ctx, match_user_id).await?;
                     let notification = format!(
                         "Restricted [{}]({}) for creating a multiaccount [{}]({})",
-                        match_username, match_user_id, username, user_id,
+                        match_username, match_profile_link, username, user_profile_link,
                     );
                     let _ =
                         discord::send_logs_red_embed("User restricted", &notification, None).await;
                 }
                 false => {
                     let notification = format!(
-                        "[{match_username}]({match_user_id}) created multiaccount [{username}]({user_id})",
+                        "[{match_username}]({match_profile_link}) created multiaccount [{username}]({user_profile_link})",
                     );
                     let _ = discord::send_logs_red_embed(
                         "User created multiaccount",
@@ -123,7 +127,7 @@ async fn do_verification_checks<C: Context>(
                 hw_match.total_occurrences as f32 / hw_matches.total_hardware_matches as f32;
             let notification = format!(
                 "[{}]({}) may have created a multiaccount [{}]({})\nHardware Usage Percentage: {:.2}%",
-                match_username, match_user_id, username, user_id, usage_percent,
+                match_username, match_profile_link, username, user_profile_link, usage_percent,
             );
             let _ = discord::send_hw_red_embed(
                 "Possible multiaccount association",
@@ -144,14 +148,17 @@ async fn do_regular_checks<C: Context>(
     client_hashes: &ClientHashes,
     hw_matches: AggregateMatchingHardwareResult,
 ) -> ServiceResult<()> {
-    let user_hw_entry = fetch_self_aggregate_match(ctx, user_id, false, client_hashes).await?;
+    let user_profile_link = website::get_profile_link(user_id);
 
+    let user_hw_entry = fetch_self_aggregate_match(ctx, user_id, false, client_hashes).await?;
     let total_hardware_usages =
         (hw_matches.total_hardware_matches + user_hw_entry.total_occurrences) as f32;
     let usages = user_hw_entry.total_occurrences as f32;
     let usage_percent = usages / total_hardware_usages;
+
     for (match_user_id, hw_match) in hw_matches.user_matches {
         let match_username = &hw_match.username;
+        let match_profile_link = website::get_profile_link(match_user_id);
         let match_usage_percent = hw_match.total_occurrences as f32 / total_hardware_usages;
         if hw_match.has_activated_hardware {
             if hw_match.user_privileges.is_publicly_visible() {
@@ -159,10 +166,10 @@ async fn do_regular_checks<C: Context>(
                     let notification = format!(
                         "[{}]({}) ({:.2}%) has logged in with [{}]({}) ({:.2}%)'s hardware too often!",
                         username,
-                        user_id,
+                        user_profile_link,
                         usage_percent * 100.0,
                         match_username,
-                        match_user_id,
+                        match_profile_link,
                         match_usage_percent * 100.0,
                     );
                     let _ =
@@ -170,7 +177,7 @@ async fn do_regular_checks<C: Context>(
                             .await;
                 } else {
                     let notification = format!(
-                        "[{username}]({user_id}) logged in with [{match_username}]({match_user_id})'s hardware."
+                        "[{username}]({user_profile_link}) logged in with [{match_username}]({match_profile_link})'s hardware."
                     );
                     let _ = discord::send_hw_blue_embed(
                         "User logged in with another users' hardware",
@@ -182,7 +189,7 @@ async fn do_regular_checks<C: Context>(
             } else {
                 // users::ban(ctx, user_id).await?;
                 let notification = format!(
-                    "[{username}]({user_id}) logged in with [{match_username}]({match_user_id})'s hardware, who is restricted."
+                    "[{username}]({user_profile_link}) logged in with [{match_username}]({match_profile_link})'s hardware, who is restricted."
                 );
                 let _ = discord::send_logs_red_embed(
                     "Banned User (Possible Multiaccount)",
@@ -197,7 +204,7 @@ async fn do_regular_checks<C: Context>(
                 if match_usage_percent > 0.2 {
                     // users::ban(ctx, user_id).await?;
                     let notification = format!(
-                        "[{username}]({user_id}) logged in with hardware used more than 20% by [{match_username}]({match_user_id}), who is restricted."
+                        "[{username}]({user_profile_link}) logged in with hardware used more than 20% by [{match_username}]({match_profile_link}), who is restricted."
                     );
                     let _ = discord::send_logs_red_embed(
                         "Banned User (Possible Multiaccount)",
@@ -208,7 +215,7 @@ async fn do_regular_checks<C: Context>(
                     return Err(AppError::SessionsLoginForbidden);
                 } else {
                     let notification = format!(
-                        "[{username}]({user_id}) has hardware match with [{match_username}]({match_user_id}), who is restricted."
+                        "[{username}]({user_profile_link}) has hardware match with [{match_username}]({match_profile_link}), who is restricted."
                     );
                     let _ =
                         discord::send_hw_blue_embed("Possible Multiaccount", &notification, None)
@@ -218,10 +225,10 @@ async fn do_regular_checks<C: Context>(
                 let notification = format!(
                     "[{}]({}) ({:.2}%) logged in with hardware used more by [{}]({}) ({:.2}%)",
                     username,
-                    user_id,
+                    user_profile_link,
                     usage_percent * 100.0,
                     match_username,
-                    match_user_id,
+                    match_profile_link,
                     match_usage_percent * 100.0,
                 );
                 let _ = discord::send_hw_blue_embed(
