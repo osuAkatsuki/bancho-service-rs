a8e34fe1f952046451e4fd037244f6c92d82bf5f
Julius <41292439+infernalfire72@users.noreply.github.com>
2026-01-06 19:46:44 +0100

    le fixus (#31)

--- src/usecases/tillerino.rs
+++ src/usecases/tillerino.rs
@@ -12,13 +12,18 @@ pub async fn save_np<C: Context>(
     np: NowPlayingMessage<'_>,
 ) -> ServiceResult<NowPlayingState> {
     let beatmap = beatmaps_service::fetch_by_id(np.beatmap_id).await?;
+    let mode = if beatmap.mode != 0 {
+        beatmap.mode
+    } else {
+        np.mode as _
+    };
     let last_np = NowPlayingState {
         beatmap_id: np.beatmap_id,
         beatmap_set_id: beatmap.beatmapset_id,
         beatmap_md5: beatmap.beatmap_md5,
         beatmap_song_name: beatmap.song_name,
         beatmap_max_combo: beatmap.max_combo,
-        mode: np.mode as _,
+        mode: mode,
         mods: np.mods.bits(),
     };
     match tillerino::save_np(ctx, session_id, last_np).await {
